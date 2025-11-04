0c64639295feda2f78a40454422233c3909774d2
initdc <initd@outlook.com>
2025-11-04 23:48:42 +0800

    fix: it should be {repo}, and remove az

--- set-remote.rb
+++ set-remote.rb
@@ -2,7 +2,7 @@
 
 ALIAS_SSH_URL = {
   tea: "git@gitea.com:initd/{repo}.git",
-  berg: "ssh://git@codeberg.org/initd/mirror-to-gitlab.git",
+  berg: "ssh://git@codeberg.org/initd/{repo}.git",
 
   lab: "git@gitlab.com:initdc/{repo}.git",
   deb: "git@salsa.debian.org:o9/{repo}.git",
@@ -10,7 +10,7 @@ ALIAS_SSH_URL = {
   hub: "git@github.com:initdc/{repo}.git",
   bit: "git@bitbucket.org:initdc/{repo}.git",
   # sf: "ssh://initd@git.code.sf.net/p/hello-sourceforge/code",
-  az: "git@ssh.dev.azure.com:v3/asia-pacific/proj/proj"
+  # az: "git@ssh.dev.azure.com:v3/asia-pacific/proj/proj"
 }.freeze
 
 REPO = Dir.pwd.split("/").last
