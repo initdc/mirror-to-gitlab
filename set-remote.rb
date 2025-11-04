4a542cf406cc3f0e2e6cf80e969f572777419ef8
initdc <initd@outlook.com>
2025-11-04 20:21:13 +0800

    feat: add set-remote.rb, also can push codebase

---
+++ set-remote.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+ALIAS_SSH_URL = {
+  tea: "git@gitea.com:initd/{repo}.git",
+  berg: "ssh://git@codeberg.org/initd/mirror-to-gitlab.git",
+
+  lab: "git@gitlab.com:initdc/{repo}.git",
+  deb: "git@salsa.debian.org:o9/{repo}.git",
+
+  hub: "git@github.com:initdc/{repo}.git",
+  bit: "git@bitbucket.org:initdc/{repo}.git",
+  # sf: "ssh://initd@git.code.sf.net/p/hello-sourceforge/code",
+  az: "git@ssh.dev.azure.com:v3/asia-pacific/proj/proj"
+}.freeze
+
+REPO = Dir.pwd.split("/").last
+
+def ps(cmd)
+  puts cmd
+  system(cmd)
+end
+
+ALIAS_SSH_URL.each do |as, url|
+  puts "----Runing for #{as} ---"
+  url = url.sub("{repo}", REPO)
+  ps "git remote remove #{as}"
+  ps "git remote add #{as} #{url}"
+  ps "git push #{as} master -f"
+  puts "------------------------"
+  puts
+end
